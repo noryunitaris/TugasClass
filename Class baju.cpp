41004022ab4baf7d321ce1eaee5393d29b3761da
noryunitaris <yunitarisnaida@gmail.com>
2018-01-16 21:10:47 -0800

    Macam class baju wanita

--- Class baju.cpp
+++ Class baju.cpp
@@ -14,4 +14,13 @@ void bajuwanita::Harga(string total){
 	cout<< " Harga :"<< total<< endl;
 }
 int main (){
+	bajuwanita blus, shackdress, longdress;
+	
+	blus.jumlhbaju = 5;
+	blus.allsize = "P :70, LB :(6-10), LD :(87-93)";
+	shackdress.jumlhbaju = 11;
+	shackdress.allsize = "P :(90-94), LB :(6-10), LD :(87-93)";
+	longdress.jumlhbaju = 8;
+	longdress.allsize = "P :110, LB :(6-10), LD :(87-93)";
+	
 }
