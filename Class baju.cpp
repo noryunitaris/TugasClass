9e0cab4fc9010dbbcd814bbc4fec855a3c0f1898
noryunitaris <yunitarisnaida@gmail.com>
2018-01-16 21:12:03 -0800

    Kodingan macam class baju wanita

--- Class baju.cpp
+++ Class baju.cpp
@@ -23,4 +23,25 @@ int main (){
 	longdress.jumlhbaju = 8;
 	longdress.allsize = "P :110, LB :(6-10), LD :(87-93)";
 	
+	cout << "Blus : "<<endl;
+	cout << "Jumlah Baju: "<<blus.jumlhbaju<<" kodi"<<endl;
+	cout << "Allsize : "<<blus.allsize<<endl;
+	
+	blus.Harga("Rp.150.000");
+	
+	cout << ""<<endl;
+	
+	cout << "Shackdress: "<<endl;
+	cout << "Jumlah Baju: "<<shackdress.jumlhbaju<<" kodi"<<endl;
+	cout << "Allsize : "<<shackdress.allsize<<endl;
+	
+	shackdress.Harga("Rp.300.000");
+	
+	cout << ""<<endl;
+	
+	cout << "Longdress : "<<endl;
+	cout << "Jumlah Baju: "<<longdress.jumlhbaju<<" kodi"<<endl;
+	cout << "Allsize : "<<longdress.allsize<<endl;
+	
+	longdress.Harga("Rp.500.000");	
 }
