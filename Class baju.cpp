f9eaf451a34fd16ace8f8dbecb5de7cf95777e36
noryunitaris <yunitarisnaida@gmail.com>
2018-01-16 21:09:46 -0800

    Inputan Class baju wanita

--- Class baju.cpp
+++ Class baju.cpp
@@ -2,6 +2,16 @@
 #include <string>
 
 using namespace std;
+class bajuwanita{
+		public :
+			int jumlhbaju;
+			string allsize;
+			void Harga(string total);
+};
 
+void bajuwanita::Harga(string total){
+	
+	cout<< " Harga :"<< total<< endl;
+}
 int main (){
 }
